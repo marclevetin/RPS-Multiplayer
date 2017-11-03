5a980855cfb783d901d70f02dc48169a3ec6cb1d
marclevetin <marc@derbyhat.org>
2017-11-03 00:48:01 -0400

    add firebase to index.html and app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCd9fS65x4GmbvHTS5qAJ6nQMzN8LYJDKE",
+  authDomain: "rock-paper-scissors-multi.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-multi.firebaseio.com",
+  projectId: "rock-paper-scissors-multi",
+  storageBucket: "",
+  messagingSenderId: "1038671516504"
+};
+firebase.initializeApp(config);
